6737fd2d37aa283456e4209b357d79e5571a4c82
David Mendez <davidmendezrosa@gmail.com>
2024-07-09 19:47:33 -0400

    navbar homepage fully working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-// import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS globally
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS globally
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,6 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
       </body>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     </html>
   );
 }
