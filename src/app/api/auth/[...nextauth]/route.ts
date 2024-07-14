7e5b0c5c0e1777952cb1e21dd743671b90c8bdcc
David Mendez <davidmendezrosa@gmail.com>
2024-07-14 01:09:46 -0400

    google auth not working in production

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
-// console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
-// console.log("GOOGLE_SECRET:", process.env.GOOGLE_SECRET);
-// console.log("SECRET:", process.env.SECRET);
+console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
+console.log("GOOGLE_SECRET:", process.env.GOOGLE_SECRET);
+console.log("SECRET:", process.env.SECRET);
 
 const authOptions = {
     providers: [
