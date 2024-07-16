e584b120ae23e68b9b580dfe52e8f7e316a8b32d
David Mendez <davidmendezrosa@gmail.com>
2024-07-16 19:01:07 -0400

    tailwind in dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import SideNav from "../../components/sideNav";
+import  SideNav from "../../components/sideNav";
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS globally
 import "../../styles/globals.css";
-export const experimental_ppr = true;
 
-const inter = Inter({ subsets: ["latin"] });
+
 
 export const metadata: Metadata = {
   title: "HustleHoopersPR",
   description: "Dashboard"
 };
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
     children,
@@ -18,11 +19,11 @@ export default function RootLayout({
     children: React.ReactNode;
   }>) {
     return (
-      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-        <div className="w-full flex-none md:w-64">
-          <SideNav />
+      <div className="flex h-screen">
+        <SideNav />
+        <div className="flex-grow p-6 bg-gray-50 overflow-auto">
+          {children}
         </div>
-        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
       </div>
     );
   }
