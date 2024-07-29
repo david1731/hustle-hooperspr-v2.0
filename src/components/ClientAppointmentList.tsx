4b9902d98313fd416d1468664b569b68124cc3e8
David Mendez <davidmendezrosa@gmail.com>
2024-07-29 01:57:40 -0400

    more styling

--- src/components/ClientAppointmentList.tsx
+++ src/components/ClientAppointmentList.tsx
@@ -22,15 +22,15 @@ const ClientAppointmentsList: React.FC<AppointmentsListProps> = ({ appointments 
   };
 
   return (
-    <div className="container mt-5">
-      <h1 className="mb-4">Your Appointments</h1>
+    <div className="mt-5">
+      <h1 className="mb-4 text-4xl antialiased">Your Appointments</h1>
       {appointments.length === 0 ? (
         <p>No appointments found.</p>
       ) : (
-        <div className="row">
+        <div className="flex flex-wrap justify-start">
           {appointments.map(appointment => (
-            <div key={appointment.app_id} className="col-md-4 mb-4">
-              <div className="card">
+            <div key={appointment.app_id} className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+              <div className="card bg-white shadow-md hover:shadow-lg transition-shadow duration-300">
                 <div className="card-body">
                   <h5 className="card-title">Entrenador: {appointment.trainer_fullname}</h5>
                   <h6 className="card-subtitle mb-2 text-muted">Servicio: {appointment.service}</h6>
@@ -39,8 +39,8 @@ const ClientAppointmentsList: React.FC<AppointmentsListProps> = ({ appointments 
                     <strong>Hora:</strong> {appointment.starttime} - {appointment.endtime}<br />
                     <strong>Fecha:</strong> {appointment.appointment_date}
                   </p>
-                  <Button type="button" className="btn btn-danger" onClick={() => handleCancel(appointment.app_id)}>Cancelar Cita</Button>
-                  <Button type="button" className="btn btn-success ml-2" onClick={() => router.push(`/dashboard/citas/${appointment.app_id}`)}>Editar Cita</Button>
+                  <Button type="button" className="btn btn-danger mt-2" onClick={() => handleCancel(appointment.app_id)}>Cancelar Cita</Button>
+                  <Button type='button' className='btn btn-success ml-2 mt-2' onClick={() => router.push(`/dashboard/citas/${appointment.app_id}`)}>Editar Cita</Button>
                 </div>
               </div>
             </div>
