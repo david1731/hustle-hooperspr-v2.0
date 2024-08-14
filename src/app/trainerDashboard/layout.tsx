5fc3a925cf9a98378b8d8afea274752bfb1b8cba
David Mendez <davidmendezrosa@gmail.com>
2024-08-14 14:52:02 -0400

    creating trainer Dashboard

---
+++ src/app/trainerDashboard/layout.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode } from 'react';
+import Link from 'next/link';
+import SideNav from "../../components/trainerSideNav";
+import "../../styles/globals.css";
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex h-screen flex-col md:flex-row">
+        <div className="w-full flex-none md:w-64">
+          <SideNav />
+        </div>
+        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+          {children}
+        </div>
+      </div>
+  );
+};
+
+
+
+export default Layout;
+
+
+
+
