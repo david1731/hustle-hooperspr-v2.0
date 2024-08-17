5b3cf944bb76ab37cc505e45776f55cc9358f484
David Mendez <davidmendezrosa@gmail.com>
2024-08-16 23:26:10 -0400

    trainer can now see their appointments

--- src/app/trainerSignin/[trainer_id]/trainerDashboard/citas/page.tsx
+++ src/app/trainerSignin/[trainer_id]/trainerDashboard/citas/page.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import AppointmentsList from "@/components/TrainerAppList";
 
-export default function Page(){
+interface PageProps {
+    params: {
+      trainer_id: string;
+    };
+}
+  
+export default function Page({ params }: PageProps){
+    const trainer_id = Number(params.trainer_id);
+    if (isNaN(trainer_id)) {
+        return <p>Invalid Trainer ID</p>;
+    }
     return(
-        <h1>Trainer Citas</h1>
+       <>
+         <h1>Trainer Citas</h1>
+         <AppointmentsList trainer_id={trainer_id}/>
+       </>
     )
 }
---
+++ src/components/TrainerAppList.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { trainerAppointments } from '@/app/lib/data';
+import { AppointmentQueryResult } from '@/app/lib/definitions';
+import TrainerAppointmentsList from './TrainerAppointments';
+
+export default async function AppointmentsList({ trainer_id }: { trainer_id: number}) {
+  let appointments: AppointmentQueryResult[] = [];
+
+  try {
+    appointments = await trainerAppointments(trainer_id);
+    console.log("Appointments in AppointmentsList:", appointments);
+  } catch (error) {
+    console.error('Error fetching appointments:', error);
+  }
+
+  return <TrainerAppointmentsList appointments={appointments} />;
+}
