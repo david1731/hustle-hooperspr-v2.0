f8c7abe3fac5bed2486208491bcac510d9b7b493
David Mendez <davidmendezrosa@gmail.com>
2024-08-25 18:41:03 -0400

    database mods

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,89 +1,62 @@
-import { drizzle } from 'drizzle-orm/vercel-postgres';
-import { sql } from '@vercel/postgres';
-import {
-  pgTable,
-  serial,
-  text,
-  integer,
-  uniqueIndex,
-} from 'drizzle-orm/pg-core';
- 
-export const ClientsTable = pgTable(
-    'clients',
-    {
-        client_id: serial('id').primaryKey(),
-        fullname: text('fullname').notNull(),
-        email: text('email').notNull(),
-        
-    },
-    (clients) => {
-        return {
-            uniqueIdx: uniqueIndex('unique_client_idx').on(clients.email),
-        };
-    },
-);
+import { pgTable, serial, text, foreignKey, integer, varchar, uniqueIndex } from "drizzle-orm/pg-core"
+import { sql } from "drizzle-orm"
 
-export const TrainersTable = pgTable(
-    'trainers',
-    {
-        trainer_id: serial('trainer_id').primaryKey(),
-        fullname: text('fullname').notNull(),
-        email: text('email').notNull(),
-    },
-    (trainers) => {
-        return {
-            uniqueIdx: uniqueIndex('unique_trainer_idx').on(trainers.email),
-        };
-    },
-);
 
-export const ServicesTable = pgTable(
-    'services',
-    {
-        service_id: serial('service_id').primaryKey(),
-        servicename: text('servicename').notNull(),
-        description: text('description').notNull(),
-    },
-);
 
-export const TimeSlotsTable = pgTable(
-    'time_slots',
-    {
-        slot_id: serial('slot_id').primaryKey(),
-        starttime: text('start_time').notNull(),
-        endtime: text('endtime').notNull(),
-    },
-);
+export const levels = pgTable("levels", {
+	level_id: serial("level_id").primaryKey().notNull(),
+	level: text("level").notNull(),
+	description: text("description").notNull(),
+});
 
-export const LevelsTable = pgTable(
-    'levels',
-    {
-        level_id: serial('level_id').primaryKey(),
-        level: text('level').notNull(),
-        description: text('description').notNull(),
-    },
-);
+export const appointment_slots = pgTable("appointment_slots", {
+	app_id: integer("app_id").primaryKey().notNull(),
+	slot_id: integer("slot_id").references(() => time_slots.slot_id),
+	client_id: integer("client_id").references(() => clients.id),
+	level_id: integer("level_id").references(() => levels.level_id),
+	trainer_id: integer("trainer_id").references(() => trainers.trainer_id),
+	service_id: integer("service_id").references(() => services.service_id),
+	date: text("date").notNull(),
+	paidstatus: varchar("paidstatus", { length: 20 }),
+});
 
-export const TrainerTimeSlotsTable = pgTable( //should contain the trainer and the slots he has available
-    'trainer_time_slots',
-    {
-      trainer_id: integer('trainer_id').references(() => TrainersTable.trainer_id),
-      slot_id: integer('slot_id').references(() => TimeSlotsTable.slot_id),
-      status: text('status').notNull(),
-      date: text('date').notNull(),
-    },
-);   
+export const services = pgTable("services", {
+	service_id: serial("service_id").primaryKey().notNull(),
+	servicename: text("servicename").notNull(),
+	description: text("description").notNull(),
+});
 
-export const AppointmentSlotsTable = pgTable(
-    'appointment_slots',
-    {
-      app_id: serial('app_id').primaryKey(),
-      slot_id: integer('slot_id').references(() => TimeSlotsTable.slot_id),
-      client_id: integer('client_id').references(() => ClientsTable.client_id),
-      level_id: integer('level_id').references(() => LevelsTable.level_id),
-      trainer_id: integer('trainer_id').references(() => TrainersTable.trainer_id),
-      service_id: integer('service_id').references(() => ServicesTable.service_id),
-      date: text('date').notNull(),
-      paidStatus: text('paidStatus').notNull(),
-    },
-);
+export const clients = pgTable("clients", {
+	id: serial("id").primaryKey().notNull(),
+	fullname: text("fullname").notNull(),
+	email: text("email").notNull(),
+},
+(table) => {
+	return {
+		unique_client_idx: uniqueIndex("unique_client_idx").using("btree", table.email),
+	}
+});
+
+export const time_slots = pgTable("time_slots", {
+	slot_id: serial("slot_id").primaryKey().notNull(),
+	start_time: text("start_time").notNull(),
+	endtime: text("endtime").notNull(),
+});
+
+export const trainers = pgTable("trainers", {
+	trainer_id: serial("trainer_id").primaryKey().notNull(),
+	fullname: text("fullname").notNull(),
+	email: text("email").notNull(),
+},
+(table) => {
+	return {
+		unique_trainer_idx: uniqueIndex("unique_trainer_idx").using("btree", table.email),
+	}
+});
+
+export const trainer_time_slots = pgTable("trainer_time_slots", {
+	trainer_id: integer("trainer_id").references(() => trainers.trainer_id),
+	slot_id: integer("slot_id").references(() => time_slots.slot_id),
+	status: text("status").notNull(),
+	date: text("date").notNull(),
+});
